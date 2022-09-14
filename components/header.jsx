37dec4757283e24aa805ea2f0803dc7d1cb7a913
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2022-09-14 11:43:11 -0300

    Add files via upload

--- components/header.jsx
+++ components/header.jsx
@@ -1,33 +1,34 @@
+import Image from 'next/image';
+
 export default function Header(){
     return(
         <>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg bg-light "> {/* trocar cor bg */}
+            <div className="">
+                <nav className="navbar navbar-expand-lg  cora nav"> {/* trocar cor bg */}
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="">Takout</a>  {/* colocar logo */}
+                        <a className="navbar-brand cor-letra" href=""> <Image src="/Takout.png" width={50} height={20} alt='takout'></Image> </a>
                         <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>    
                         </button>    
 
-                        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                            <ul className="navbar-nav">
+                        <div className="collapse navbar-collapse " id="navbarNav">
+                            <ul className="navbar-nav ">
                                 <li className="nav-item">
-                                    <a className="nav-link active" href="">Serviços</a>
+                                    <a className="nav-link cor-letra" href="">Serviços</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link active" href="">Solução</a>
+                                    <a className="nav-link cor-letra" href="">Solução</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link active" href="">Como Funciona</a>
+                                    <a className="nav-link cor-letra" href="">Como Funciona</a>
                                 </li>
                             </ul>
                         </div>
                     </div>  
 
-                    {/* muadr cor e texto dos botões */}
                     <div className="container-fluid justify-content-end">
-                        <button className="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
-                        <button className="btn btn-outline-success me-2" type="button">Main button</button>
+                        <button className="btn btn-sm cor-letra" type="button">Login</button>
+                        <button className="btn btn-primary me-2 cor-letra" type="button">  Iniciar  </button>
                     </div>
                 
                 </nav>    
