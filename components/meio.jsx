1eb2b7b86d34887d80d61eb07f84c4d3a07d71b0
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2022-09-15 10:12:28 -0300

    Add files via upload

---
+++ components/meio.jsx
@@ -0,0 +1,9 @@
+export default function Meio(){
+    return(
+        <>
+            <div className="grid text-center">
+                <h1 className="g-col-6 g-col-md-4">Feito simples para pequenos negócios</h1>
+            </div>
+        </>
+    );
+}
